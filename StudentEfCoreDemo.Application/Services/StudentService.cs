b3ed5fa39f47c2efa6ff44cb4ba921d24634448b
Jenna Hamarus <129057835+JennaHamarus@users.noreply.github.com>
2025-04-28 09:39:58 +0300

    Ensimmäinen commit

---
+++ StudentEfCoreDemo.Application/Services/StudentService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentEfCoreDemo.Domain.Entities;
+using StudentEfCoreDemo.Domain.Interfaces
+
+namespace StudentEfCoreDemo.Application.Services
+{
+    public class StudentService
+    {
+        private readonly IStudentRepository _studentRepository;
+        public StudentService(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        public async Task<List<Student>> GetStudentsAsync()
+        {
+            return await _studentRepository.GetAllAsync();
+        }
+
+        public async Task<Student?> GetStudentByIdAsync(int id)
+        {
+            return await _studentRepository.GetByIdAsync(id);
+        }
+
+        public async Task AddStudentAsync(StudentService student)
+        {
+            await _studentRepository.AddAsync(student);
+        }
+
+        public async Task UpdateStudentAsync(StudentService student)
+        {
+            await _studentRepository.UpdateAsync(student);
+        }
+
+        public async Task DeleteStudentAsync(int id)
+        {
+            await _studentRepository.DeleteAsync(id);
+        }
+    }
+}
---
+++ StudentEfCoreDemo.Domain/Interfaces/IStudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentEfCoreDemo.Domain.Interfaces
+{
+    internal interface IStudentRepository
+    {
+        Task<List<Student>> GetAllAsync();
+        Task<Student?> GetByIdAsync(int id);
+        Task AddAsync(Student student);
+        Task UpdateAsync(Student student);
+        Task DeleteAsync(int id);
+    }
+}
